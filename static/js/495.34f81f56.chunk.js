b9eba7f6ff4efc69f543a95ddd141167db30a065
natela-kk <n.kerd@mail.ru>
2023-07-11 11:29:07 +0000

    Updates

---
+++ static/js/495.34f81f56.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkanime_list=self.webpackChunkanime_list||[]).push([[495],{1495:function(n,i,t){t.r(i),t.d(i,{History:function(){return r}});var e=t(184);function r(){return(0,e.jsx)("div",{children:"History Component"})}}}]);
+//# sourceMappingURL=495.34f81f56.chunk.js.map
