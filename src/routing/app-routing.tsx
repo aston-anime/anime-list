bc313a968c75c4291c9102c5cf00b3019da71a72
Natela <n.kerd@mail.ru>
2023-07-18 10:47:51 +0300

    fix: error boundry

--- src/routing/app-routing.tsx
+++ src/routing/app-routing.tsx
@@ -1,6 +1,8 @@
 import {createRoutesFromElements, Route} from 'react-router';
 import {createBrowserRouter} from 'react-router-dom';
+import {ErrorBoundary} from 'react-error-boundary';
 import {Layout} from '../components/Layout/Layout';
+import {ErrorPage} from '../pages/Error-page/Error-page';
 import {AppRoute} from './AppRoute';
 import {
     DetailedItemPage,
@@ -14,7 +16,14 @@ import {
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path={AppRoute.Main} element={<Layout />}>
+        <Route
+            path={AppRoute.Main}
+            element={
+                <ErrorBoundary FallbackComponent={ErrorPage}>
+                    <Layout />
+                </ErrorBoundary>
+            }
+        >
             <Route index element={<MainPage />} />
             <Route path={AppRoute.Search} element={<SearchPage />} />
             <Route path={AppRoute.DetailedItem} element={<DetailedItemPage />} />
